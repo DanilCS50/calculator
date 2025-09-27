7a6cf98203808fe88a335a4bbfa1cd8608be75e1
Danil <danil_butenko2010@mail.ru>
2025-09-27 07:35:21 +0300

    файлы

--- calculator/advancexd.py
+++ calculator/advancexd.py
@@ -21,27 +21,4 @@ def degree(x, y):
     z = x ** y
     logger.debug("Результат возведении в стпень = %s", z)
     logger.info("Функция возвращает значение")
-    return zimport logging
-logging.basicConfig(
-    level = logging.DEBUG,
-    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt = '%Y-%m-%d %H:%M:%S'
-)
-logger = logging.getLogger(__name__)
-
-def factorial(x):
-    logger.info("Функция факториала запущена")
-    if x == 1:
-        logger.debug("Возвращение функции x == 1")
-        return x
-    else:
-        logger.debug("Рекурсия: x = %s", x)
-        return x * factorial(x-1)
-        
-
-def degree(x, y):
-    logger.info("Функция возведения в стпень запущена")
-    z = x ** y
-    logger.debug("Результат возведении в стпень = %s", z)
-    logger.info("Функция возвращает значение")
     return z
