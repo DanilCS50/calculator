3a4c5c36a55728877fcc1f1fc68b511f84d514de
Danil <danil_butenko2010@mail.ru>
2025-10-01 06:43:15 +0300

    Файл с важными командами для git

---
+++ 5.py
@@ -0,0 +1,9 @@
+list = [1, 2]
+for i in range(3, 6):
+    list.append(i)
+list.append("ab")
+
+if list < [3, 4, 5]:
+    print(True)
+else:
+    print(False)
---
+++ square.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+numbers2 = numbers[1: len(numbers): 2]
+print(numbers2)
+numbers2 = numbers[-3:]
+print(numbers2)
+numbers2 = numbers[len(numbers)::-1]
+print(numbers2)
